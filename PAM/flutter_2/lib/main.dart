18ca477e80b4190a0e3d7572217c9a71b653c6e0
Tms.exe <trentintommaso4@gmail.com>
2025-11-24 18:18:09 +0100

    Updated flutter2

--- PAM/flutter_2/lib/main.dart
+++ PAM/flutter_2/lib/main.dart
@@ -6,15 +6,19 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyScaffold extends StatefulWidget{
+class MyScaffold extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _MyScaffold();
-
 }
 
 class _MyScaffold extends State<MyScaffold> {
-  bool? _checkboxValue = false;
-  bool _switchValue = false;
+  //bool? _checkboxValue = false;
+  //bool _switchValue = false;
+  //int? _radioValue = 1;
+  String? email = "";
+  String? password = "";
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +28,7 @@ class _MyScaffold extends State<MyScaffold> {
       body: Center(
         child: Column(
           children: [
-            Padding(
+            /*Padding(
               padding: EdgeInsets.all(8),
               child: TextField(
                 decoration: InputDecoration(
@@ -74,11 +78,121 @@ class _MyScaffold extends State<MyScaffold> {
                   _switchValue = newvalue;
                 });
               },
+            ),*/
+            /*Row(
+              children: [
+                Radio(
+                  value: 1,
+                  groupValue: _radioValue,
+                  onChanged: (int? newValue){
+                    setState(() {
+                      _radioValue = newValue;
+                    });
+                  },
+                ),
+                Text("Option 1"),
+              ],
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 2,
+                  groupValue: _radioValue,
+                  onChanged: (int? newValue){
+                    setState(() {
+                      _radioValue = newValue;
+                    });
+                  },
+                ),
+                Text("Option 2"),
+              ],
+            )
+            */
+            SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {
+                _selectDate(context);
+              },
+              child: Text("Show DatePicker"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _selectTime(context);
+              },
+              child: Text("Show Time"),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: "name@company.com",
+                      labelText: "Email"
+                    ),
+                    validator: (String? inVal){
+                      if(inVal!.length == 0){
+                        return "Please enter email";
+                      }
+                      return null;
+                    },
+                    onSaved: (String? inVal){
+                      this.email = inVal;
+                    },
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "password",
+                      labelText: "Password"
+                    ),
+                    validator: (String? inVal){
+                      if(inVal!.length < 8){
+                        return "Need longer password";
+                      }
+                      return null;
+                    },
+                    onSaved: (String? inVal){
+                      this.password = inVal;
+                    },
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if(_formKey.currentState!.validate()){
+                        _formKey.currentState!.save();
+                        print("Email: " + this.email.toString());
+                        print("Password: " + this.password.toString());
+                      };
+                    },
+                    child: Text("Login"),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _selectDate(BuildContext context) async {
+    DateTime? selectedDate = await showDatePicker(
+      context: context,
+      firstDate: DateTime(2024),
+      lastDate: DateTime(2028),
+      initialDate: DateTime.now(),
+    );
+    print(selectedDate);
+  }
+
+  void _selectTime(BuildContext context) async {
+    TimeOfDay? selectedtime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+      initialEntryMode: TimePickerEntryMode.inputOnly,
+    );
+    print(selectedtime);
   }
 }
 
